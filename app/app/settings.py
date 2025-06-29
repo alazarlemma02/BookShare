e57acd8f267da1ba42acdd025a51555312f9450f
alazarlemma02 <alazar03lemma@gmail.com>
2025-06-29 20:19:34 +0300

    corrected the database setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,10 +76,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get('DB_HOST', 'localhost'),
-            'NAME': os.environ.get('DB_NAME', 'postgres'),
-            'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASS', 'postgres'),
+            'HOST': os.environ.get('DB_HOST'),
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASS'),
         }
     }
 
